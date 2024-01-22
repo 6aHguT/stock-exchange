cf28dbf178b7a2d0263530504642653e834b9ae2
bum <rublevskiy.leonid@gmail.com>
2024-01-22 11:33:12 +0300

    refactoring

--- src/main/java/ru/sbrf/stock/exchange/repository/OrderInfoRepository.java
+++ src/main/java/ru/sbrf/stock/exchange/repository/OrderInfoRepository.java
@@ -3,7 +3,6 @@ package ru.sbrf.stock.exchange.repository;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 import ru.sbrf.stock.exchange.domain.OrderInfo;
 import ru.sbrf.stock.exchange.domain.Status;
@@ -15,7 +14,6 @@ import java.util.UUID;
 @Repository
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, UUID> {
     List<OrderInfo> findAllByStatus(Status status);
-    @Modifying
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<OrderInfo> findById(UUID id);
 }
